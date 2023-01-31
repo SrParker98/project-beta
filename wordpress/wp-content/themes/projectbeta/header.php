28cdc878fa3034d485762780fafc4123cda497aa
SrParker98 <116755291+SrParker98@users.noreply.github.com>
2023-01-31 20:30:15 +0100

    1

---
+++ wordpress/wp-content/themes/projectbeta/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostecosolucion</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+          <a href="/index.html"  class="">⚛️</a>
+
+            <ul class="">
+
+            <li class="">
+              <a href="/wp-content/themes/projectbeta/_html/index.html" class="">Inicio</a>
+            </li>
+            <li class="">
+              <a href="/wp-content/themes/projectbeta/_html/blog.html" class="">Blog</a>
+            </li>
+            <li class="">
+              <a href="/wp-content/themes/projectbeta/_html/about.html" class="">Sobre mí</a>
+            </li>
+            <li class="">
+                <a href="/wp-content/themes/projectbeta/_html/kitdigital.html" class="">Kit Digital</a>
+              </li>
+            <li class="">
+              <a href="/wp-content/themes/projectbeta/_html/contacto.html" class="">Contacto</a>
+            </li>
+        
+
+          </ul>
+        </nav>
+      </header>
+      
